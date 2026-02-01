1b242c89510f55a7ec22ddaadfd76de5770c7114
Md. Mahdiul Hasan <devmahdimunshee@gmail.com>
2026-02-01 15:34:10 +0600

    Lottie Initial & Setup in Register Page

--- client/src/Pages/Auth/SignUp.jsx
+++ client/src/Pages/Auth/SignUp.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react';
+import Lottie from 'lottie-react';
+import signUpLottie from '../../assets/Lotties/SignUp.json';
 
 const SignUp = () => {
   return (
     <>
       <div className='hero bg-base-200 min-h-screen'>
         <div className='hero-content flex-col lg:flex-row-reverse'>
-          <div className='text-center lg:text-left'></div>
+          <div className='text-center lg:text-left'>
+            <Lottie
+              style={{ width: '100%' }}
+              animationData={signUpLottie}
+              loop={true}
+            />
+          </div>
           <div className='card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl'>
             <div className='card-body'>
               <h1 className='text-5xl font-bold'>Register now!</h1>
-              <fieldset className='fieldset'>
-                <label className='label'>Email</label>
-                <input
-                  type='email'
-                  className='input'
-                  placeholder='Email'
-                />
-                <label className='label'>Password</label>
-                <input
-                  type='password'
-                  className='input'
-                  placeholder='Password'
-                />
-                <div>
-                  <a className='link link-hover'>Forgot password?</a>
-                </div>
-                <button className='btn btn-neutral mt-4'>Login</button>
-              </fieldset>
+              <form>
+                <fieldset className='fieldset'>
+                  <label className='label'>Email</label>
+                  <input
+                    type='email'
+                    className='input'
+                    name='email'
+                    placeholder='Email'
+                  />
+                  <label className='label'>Password</label>
+                  <input
+                    type='password'
+                    className='input'
+                    name='password'
+                    placeholder='Password'
+                  />
+                  <div>
+                    <a className='link link-hover'>Forgot password?</a>
+                  </div>
+                  <button className='btn btn-neutral mt-4'>Register</button>
+                </fieldset>
+              </form>
             </div>
           </div>
         </div>
