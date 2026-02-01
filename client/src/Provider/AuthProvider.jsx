0dcf640042ab1d8d06bcd1a2d460da7bbf9e5776
Md. Mahdiul Hasan <devmahdimunshee@gmail.com>
2026-02-01 16:16:53 +0600

    Create User with Email & Password Setup in AuthProvider.jsx

--- client/src/Provider/AuthProvider.jsx
+++ client/src/Provider/AuthProvider.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import { AuthContext } from '../Context/AuthContext';
+import { Auth } from '../Firebase/Firebase.config';
 
 const AuthProvider = ({ children }) => {
-  const authInfo = {};
+  const [loading, setLoading] = useState(true);
+  const createUserWithEP = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(Auth, email, password);
+  };
+  const authInfo = {
+    loading,
+    createUserWithEP,
+  };
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
